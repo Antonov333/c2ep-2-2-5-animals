87f0493d79449865e51185faedacf19b651de6f7
Antonov333 <antonovsergei333@yahoo.com>
2023-04-28 22:46:07 +0300

    Animal class created

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+public abstract class Animal {
+    private String food;
+    private String location;
+    private String species;
+
+    public void makeNoise() {
+    }
+
+    public void eat() {
+    }
+
+    public void sleep() {
+    }
+
+}
